13429b166175b34bd1fe50bb6a3291026a36f10d
duane11003 <duane.mcfar@gmail.com>
2020-06-01 14:15:16 -0400

    refactored API call using axios

--- src/Components/OrdersResults.js
+++ src/Components/OrdersResults.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect, Fragment } from "react";
 import WORK_ORDER_KEY from "../../WORK_ORDER_KEY";
+import axios from "axios";
 
 function OrdersResults() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [workOrder, setWorkOrder] = useState(null);
-  const [workerID, setWorkerID] = useState(null);
+  const [workerID, setWorkerID] = useState([]);
   const [workerName, setWorkerName] = useState([]);
   const [workerCompany, setWorkerCompany] = useState([]);
+  const [workerImage, setWorkerImage] = useState("");
 
-  const getWorkOrders = async () => {
-    await fetch(WORK_ORDER_KEY)
-      .then((res) => res.json())
+  const getWorkOrders = () => {
+    axios
+      .get(WORK_ORDER_KEY)
       .then((response) => {
-        setWorkOrder(response.orders);
-        setError(false);
+        setWorkOrder(response.data.orders);
         setLoading(false);
-        console.log(response.orders);
+        setError(false);
       })
       .catch((err) => {
-        console.log(err);
         setError(true);
-        setLoading(false);
+        console.log(err);
       });
   };
 
@@ -29,35 +29,38 @@ function OrdersResults() {
     getWorkOrders();
   }, []);
 
+  const mappedWorkOrders = () => {
+    workOrder.map(({ deadline, description, id, workerId }) => {
+      axios
+        .get(`https://www.hatchways.io/api/assessment/workers/${workerId}`)
+        .then((response) => {
+          setWorkerName(response.data.name);
+          setWorkerCompany(response.data.company);
+          setWorkerImage(response.data.image);
+        });
+      <li key={id}>
+        <h2>Work Order: {id}</h2>
+        <p>Description: {description}</p>
+        <p>Deadline: {deadline}</p>
+        <p>Worker: {workerName}</p>
+        <p>{workerCompany}</p>
+      </li>;
+    });
+  };
+
+
   if (loading === true) return <h3>Loading...</h3>;
   if (error) return <p>Error fetching workers</p>;
 
-  const mappedWorkOrders = workOrder.map(
-    ({ deadline, description, id, name, workerId }) => {
-      return (
-        <div key={id} className="workOrderContainer">
-          <li key={id}>
-            <h3>Work Order: {id}</h3>
-            <p>{description}</p>
-            <div>
-              <p>Worker Id: {workerId}</p>
-            </div>
-            <p>{deadline}</p>
-          </li>
-        </div>
-      );
-    }
-  );
+  console.log(workOrder, "is the work order array");
+
+  
 
   return (
     <Fragment>
       <div>
-        <input
-         placeholder='Worker Name'
-         type='text'
-        >
-        </input>
-      <ul className='unorderedList'>{mappedWorkOrders}</ul>
+        <input placeholder="Worker Name" type="text"></input>
+        <ul className="unorderedList">{mappedWorkOrders}</ul>
       </div>
     </Fragment>
   );
